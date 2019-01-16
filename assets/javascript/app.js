f35e7ba0c568a1c36505f05e6ad3d6434f59e5d5
Jennifer Gingras <jennifergingras@tests-MacBook-Pro-2.local>
2019-01-16 17:15:14 -0500

    timer added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,8 @@ $(document).ready(function () {
   var queryURL = 'https://opentdb.com/api.php?amount=10&category=20&type=multiple';
   var correct_answer;
   var playButton = $('#playButton');
+  var message = $('#messageLog');
+  var timeSet;
 
 
 
@@ -22,20 +24,28 @@ $(document).ready(function () {
     };
   };
 
+  // STOP THE TIMER
+  // ----------------------
+  function stopTimer() {
+    clearInterval(timeSet);
+  }
+
   // CREATE A TIMER 
   // ---------------
-  var timeLeft = 20;
-  var timeSet = setInterval(function () {
-    // display the countdown
-    $('#timeCountdown').html = --timeLeft;
-    // if time runs out stop the timer and display message
-    if (timeLeft <= 0) {
-      clearInterval(timeSet);
-      $('#messageLog').html("Out of time! The correct answer is " + correct_answer);
-      // add next question button
-      $("#nextButton").append(`<button type="button" class="btn btn-dark">${"next question"}</button><br></br>`)
-    }
-  }, 1000 * 2);
+  function countDown() {
+    var timeLeft = 20;
+    timeSet = setInterval(function () {
+      // display the countdown
+      $('#timeCountdown').text('Time Left: ' + --timeLeft);
+      // if time runs out stop the timer and display message
+      if (timeLeft <= 0) {
+        clearInterval(timeSet);
+        message.html("Out of time! The correct answer is " + correct_answer);
+        // add next question button
+        $("#nextButton").append(`<button type="button" class="btn btn-dark">${"next question"}</button><br></br>`)
+      };
+    }, 1000 * 2);
+  };
 
   // RETRIEVE A SET OF QUESTIONS FROM THE API
   // --------------------------------------------
@@ -56,11 +66,11 @@ $(document).ready(function () {
     // put answers into one array
     let allAnswers = res.results[0].incorrect_answers
     allAnswers.push(res.results[0].correct_answer)
-    console.log(allAnswers)
+    console.log(allAnswers);
     // display question in html
     $("#quizQuestion").html(res.results[0].question);
     // set timer
-
+    countDown();
     // shuffle the array
     shuffleAnswers(allAnswers);
     // loop through the array of answers to create a button for each
@@ -81,30 +91,25 @@ $(document).ready(function () {
   // CHECK IF CHOSEN ANSWER IS CORRECT  (this doesn't work yet because I need to define the correct answer)
   // ----------------------------------
   $("body").on("click", ".answerBtn", function () {
+    stopTimer();
     selectedAnswer = $('this').text();
     if (selectedAnswer === correct_answer) {
       rightAnswer++
       console.log('Correct!')
-      $('#messageLog').html("Yes! You're right!");
+      message.html("Yes! You're right!");
       //add next question button
       $("#nextButton").append(`<button type="button" class="btn btn-dark">${"next question"}</button><br></br>`)
     } else {
       wrongAnswer++
       console.log('Nope!');
-      $('#messageLog').html("Nope! The correct answer is " + correct_answer);
+      message.html("Nope! The correct answer is " + correct_answer);
       //add next question button
       $("#nextButton").append(`<button type="button" class="btn btn-dark">${"next question"}</button><br></br>`)
     };
   });
 
-
-
-
-
-
   // NEXT QUESTION NEEDS TO RUN THROUGH ARRAY
   // var nextQuestion = function (array) {
-  //   $('.changeText').empty();
   // move to next index in array of 10 questions
   // }
 
@@ -112,8 +117,13 @@ $(document).ready(function () {
   // After 10 questions display the final results
   // display number of correct answers and incorrect answers
   // $('.changeText').empty();
-
+  // $('.finalResults').text('That's it! How did you do?);
+  // $('#correctResults').text(rightAnswer + " Correct");
+  // $('#incorrectResults').text(wrongAnswer + " Incorrect");
   // Ask if user wants to play again
+  // message.html('Want to try again?');
+  // playButton.show();
+
 
 
 

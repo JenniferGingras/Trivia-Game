265d763f7b008f322a7c25da312d03771417dfd2
Jennifer Gingras <jennifergingras@tests-MacBook-Pro-2.local>
2019-01-16 14:49:42 -0500

    added more functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,15 +8,12 @@ $(document).ready(function () {
   var wrongAnswer = 0;
   // stores url for retrieving trivia questions
   var queryURL = 'https://opentdb.com/api.php?amount=10&category=20&type=multiple';
-  // will store the correct and incorrect answers so that they can be shuffled into new positions each time
-  var answerArr = [];
-  var btn = $(".btn");
-  var selectedAnswer;
   var correct_answer;
-  var questionCount = 0;
+  var playButton = $('#playButton');
 
 
-  // FUNCTION THAT SHUFFLES THE ELEMENTS IN THE ANSWER ARRAY
+
+  // SHUFFLE THE ELEMENTS IN THE ANSWER ARRAY
   // --------------------------------------------------------
   var shuffleAnswers = function (array) {
     for (var i = array.length - 1; i > 0; i--) {
@@ -25,14 +22,30 @@ $(document).ready(function () {
     };
   };
 
+  // CREATE A TIMER 
+  // ---------------
+  var timeLeft = 20;
+  var timeSet = setInterval(function () {
+    // display the countdown
+    $('#timeCountdown').html = --timeLeft;
+    // if time runs out stop the timer and display message
+    if (timeLeft <= 0) {
+      clearInterval(timeSet);
+      $('#messageLog').html("Out of time! The correct answer is " + correct_answer);
+      // add next question button
+      $("#nextButton").append(`<button type="button" class="btn btn-dark">${"next question"}</button><br></br>`)
+    }
+  }, 1000 * 2);
+
   // RETRIEVE A SET OF QUESTIONS FROM THE API
-  // --------------------------------------------------------------
+  // --------------------------------------------
   var startGame = function (url) {
     $.ajax({
       url: url,
       method: "GET"
     }).then(function (response) {
-      createBoxes(response)
+      createBoxes(response);
+      console.log(response);
     });
   }
 
@@ -44,10 +57,12 @@ $(document).ready(function () {
     let allAnswers = res.results[0].incorrect_answers
     allAnswers.push(res.results[0].correct_answer)
     console.log(allAnswers)
-    // shuffle the array
-    shuffleAnswers(allAnswers);
     // display question in html
     $("#quizQuestion").html(res.results[0].question);
+    // set timer
+
+    // shuffle the array
+    shuffleAnswers(allAnswers);
     // loop through the array of answers to create a button for each
     for (var i = 0; i < allAnswers.length; i++) {
       $(".multiChoice").append(`<button type="button" class="btn btn-outline-light answerBtn">${letters[i]}</button><span id="answerA">${allAnswers[i]}</span><br></br>`)
@@ -57,12 +72,13 @@ $(document).ready(function () {
 
   // START THE QUIZ
   // -----------------
-  $('#playButton').on('click', function () {
+  playButton.on('click', function () {
     startGame(queryURL);
+    playButton.hide();
     console.log('start');
   });
 
-  // CHECK IF CHOSEN ANSWER IS CORRECT
+  // CHECK IF CHOSEN ANSWER IS CORRECT  (this doesn't work yet because I need to define the correct answer)
   // ----------------------------------
   $("body").on("click", ".answerBtn", function () {
     selectedAnswer = $('this').text();
@@ -70,29 +86,33 @@ $(document).ready(function () {
       rightAnswer++
       console.log('Correct!')
       $('#messageLog').html("Yes! You're right!");
+      //add next question button
+      $("#nextButton").append(`<button type="button" class="btn btn-dark">${"next question"}</button><br></br>`)
     } else {
       wrongAnswer++
       console.log('Nope!');
       $('#messageLog').html("Nope! The correct answer is " + correct_answer);
+      //add next question button
+      $("#nextButton").append(`<button type="button" class="btn btn-dark">${"next question"}</button><br></br>`)
     };
   });
 
 
-  // set a timer to countdown for each question
-  // if time runs out dispaly message "Out of time! the answer is (correct answer)"
-  // wait a few seconds and then display next question
 
 
-  // next question
-  var nextQuestion = function (event) {
-    $('.dynamicText').empty();
-    startGame(queryURL);
-  }
+
+
+  // NEXT QUESTION NEEDS TO RUN THROUGH ARRAY
+  // var nextQuestion = function (array) {
+  //   $('.changeText').empty();
+  // move to next index in array of 10 questions
+  // }
 
   // END OF GAME
-
   // After 10 questions display the final results
-  // dispaly number of correct answers and incorrect answers
+  // display number of correct answers and incorrect answers
+  // $('.changeText').empty();
+
   // Ask if user wants to play again
 
 
